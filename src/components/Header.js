e1d3ebc68df460c495fe0c5851daecabb6315ff7
Marion-Lotito <marion.lotito@gmail.com>
2018-07-23 21:45:56 +0200

    feat: start to add GoogleMapReact

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ class Header extends Component {
   render() {
     return (
         <header className="App-header">
-          <h1 className="App-title">10 beautiful places to see in Dijon, Burgundy</h1>
-        </header>  
+          <h1 className="App-title">10 beautiful places to see in Dijon</h1>
+        </header>
     );
   }
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
 class Map extends Component {
+  static defaultProps = {
+    center: {
+      lat: 47.322047,
+      lng: 5.04148
+    },
+    zoom: 12,
+  };
+
   render() {
     return (
-        <p>This is the Map container</p>
+      <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{
+          key: 'AIzaSyAnAjwwoEN3ySjRoMvE5664Jw-SpMYBRiY',
+        }}
+      >
+      </GoogleMapReact>
+      </div>
     );
   }
 }

e1161a4132ea6c35c9b5bc701578de82fe7799b8
Marion-Lotito <marion.lotito@gmail.com>
2018-07-23 19:08:21 +0200

    feat: add header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+        <header className="App-header">
+          <h1 className="App-title">10 beautiful places to see in Dijon, Burgundy</h1>
+        </header>  
+    );
+  }
+}
+
+export default Header;

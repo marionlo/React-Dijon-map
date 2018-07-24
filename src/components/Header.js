18eecc605c0de8ea987e70af36bd1b779ef2f752
Marion <marion.lotito@gmail.com>
2018-07-24 17:22:07 +0200

    Update header with hamburger menu
    Update header with hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,18 +5,10 @@ class Header extends Component {
   render() {
     return (
       <header className="App-header">
-        <div className="App-nav-left">
-          <Button>
-            <Glyphicon
-              glyph="hamburger-menu"
-              className="App-Menu-Icon"
-              aria-label="Toggle Side Panel"
-            />
-          </Button>
-        </div>
-        <div className="App-nav-right">
-          <h1 className="App-title">10 beautiful places to see in Dijon</h1>
-        </div>
+        <Button className="App-Menu-icon">
+          <Glyphicon glyph="menu-hamburger" aria-label="Toggle Side Panel" />
+        </Button>
+        <h1 className="App-title">10 beautiful places to see in Dijon</h1>
       </header>
     );
   }

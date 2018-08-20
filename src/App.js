3620f8c9a0af7bb0d9213a93c5d5545ef43ddddc
Marion <marion.lotito@gmail.com>
2018-08-20 19:49:57 +0200

    Implement Flickr API

--- src/App.js
+++ src/App.js
@@ -5,13 +5,6 @@ import LocationsList from "./components/LocationsList.js";
 import "./App.css";
 
 class App extends Component {
-  /*componentDidMount() {
-    fetch(
-      "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=5fbf4edfc2e62e88f19e1e2021ac937a&tags=nyc&per_page=10&page=1&format=json&nojsoncallback=1"
-    ).then(function(response) {
-      return response.json();
-    });
-  } */
   constructor() {
     super();
     this.onFilterLocations = this.onFilterLocations.bind(this);
@@ -25,9 +18,9 @@ class App extends Component {
         key: "halles"
       },
       {
-        title: "Église Saint-Bégnigne",
+        title: "Église Saint-Bénigne",
         location: { lat: 47.3214917, lng: 5.0345168 },
-        key: "begnigne"
+        key: "benigne"
       },
       {
         title: "Hôtel de Vogüé",
@@ -75,8 +68,92 @@ class App extends Component {
     showingInfoWindow: false,
     activeMarker: {},
     selectedPlace: [],
-    filteredLocations: []
+    pictures: [],
+    filteredLocations: [
+      {
+        title: "Halles du marché",
+        location: { lat: 47.3237863, lng: 5.0399184 },
+        key: "halles"
+      },
+      {
+        title: "Église Saint-Bénigne",
+        location: { lat: 47.3214917, lng: 5.0345168 },
+        key: "benigne"
+      },
+      {
+        title: "Hôtel de Vogüé",
+        location: { lat: 47.3226771, lng: 5.0421956 },
+        key: "vogue"
+      },
+      {
+        title: "Église Notre-Dame de Dijon",
+        location: { lat: 47.3227477, lng: 5.0412288 },
+        key: "notredame"
+      },
+      {
+        title: "Jardin Darcy",
+        location: { lat: 47.3243954, lng: 5.0319993 },
+        key: "darcy"
+      },
+      {
+        title: "Porte Guillaume",
+        location: { lat: 47.3232856, lng: 5.0347209 },
+        key: "guillaume"
+      },
+      {
+        title: "Palais des ducs de Bourgogne",
+        location: { lat: 47.321427, lng: 5.0423652 },
+        key: "palais"
+      },
+      {
+        title: "Jardin botanique de l'Arquebuse",
+        location: { lat: 47.3212274, lng: 5.0276789 },
+        key: "Arquebuse"
+      },
+      {
+        title: "Église Saint-Michel",
+        location: { lat: 47.3211217, lng: 5.0449197 },
+        key: "michel"
+      },
+      {
+        title: "Musée de la vie bourguignonne Perrin de Puycousin",
+        location: { lat: 47.3174169, lng: 5.0376904 },
+        key: "musee"
+      }
+    ]
   };
+
+  componentDidMount() {
+    fetch(
+      "https://api.flickr.com/services/rest/?method=" +
+        "flickr.photos.search&api_key=5fbf4edfc2e62e88f19e1e2021ac937a" +
+        "&content_type=1&per_page=1&tags=" +
+        this.state.locations.title +
+        "&sort=relevance&format=json&nojsoncallback=1"
+    )
+      .then(function(response) {
+        return response.json();
+      })
+      .then(
+        function(j) {
+          let picArray = j.photos.photo.map(pic => {
+            var srcPath =
+              "https://farm" +
+              pic.farm +
+              ".staticflickr.com/" +
+              pic.server +
+              "/" +
+              pic.id +
+              "_" +
+              pic.secret +
+              ".jpg";
+            return <img alt={this.state.locations.title} src={srcPath} />;
+          });
+          this.setState({ pictures: picArray });
+        }.bind(this)
+      );
+  }
+
   onFilterLocations(newLocations) {
     console.log("test filter");
     this.setState({
@@ -112,7 +189,9 @@ class App extends Component {
             marker={this.state.marker}
             onMarkerClick={this.onMarkerClick}
             onFilterLocations={this.onFilterLocations}
+            filteredLocations={this.state.filteredLocations}
           />
+
           <div role="application" ref="map">
             <MapContent
               locations={this.state.locations}
@@ -122,6 +201,7 @@ class App extends Component {
               onMarkerClick={this.onMarkerClick}
               onMapClicked={this.onMapClicked}
               filteredLocations={this.state.filteredLocations}
+              pictures={this.state.pictures}
             />
           </div>
         </div>

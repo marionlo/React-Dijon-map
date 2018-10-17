063a9e18d1675dd73a94322909969c7ce0b3f812
Marion <marion.lotito@gmail.com>
2018-10-17 14:10:55 +0200

    Update Flickr API

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,8 +8,8 @@ class MapContent extends Component {
     newmarkers: []
   };
 
-  componentWillMount() {
-    let search = this.props.activeMarker.title;
+  componentWillUpdate() {
+    let search = this.props.selectedPlace.name;
     fetch(
       "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=5fbf4edfc2e62e88f19e1e2021ac937a" +
         "&content_type=1&per_page=1&text=" +
